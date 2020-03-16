0d90cb14894c592f77ac4ad4ba5a33c16ef5956d
damprog <damprog.one@gmail.com>
2020-03-16 12:24:38 +0100

    usprawnienie 0.2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     cout << "--------------------------------------------------------------------------------------------------------------------------";
     cout << endl <<"Hello, World!";
-    cout << "\n\nPrzeżywamy trudny okres epidemii..\nW tych trudnych chwilach powinniśmy spędzac czas w domu. Z tego powodu proszę o podanie 5 przykładow, co mozna robic zeby czas szybciej mijal...";
+    cout << "\n\nPrzeżywamy trudny okres epidemii..\nW tych trudnych chwilach powinnismy spędzac czas w domu. Z tego powodu prosze o podanie 5 przykladow, co mozna robic zeby czas szybciej mijal...";
     cout << "\nPodaj 5 przykladow takich czynnosci, a bede Ci wdzieczny :)"<< endl << endl;
     przyklady();
     return 0;
