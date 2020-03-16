9fc9e65552ae37b76e54c859da7e5e9c5f3b8097
damprog <damprog.one@gmail.com>
2020-03-16 11:46:51 +0100

    Dodanie tekstu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <windows.h>
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "Przeżywamy trudny okres epidemii..\n W tych trudnych chwilach powinniśmy spędzaczas w domu. Z tego powodu proszę o podanie 5 przykładow co mozna robic zeby czas szybciej mijal...";
+    cout << "\n Podaj 5 przykladow takich czynnosci a bede ci wdzieczny :)";
     return 0;
 }
