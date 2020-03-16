9130440848e88a99a1fce27f61061e7138b550b6
damprog <damprog.one@gmail.com>
2020-03-16 12:02:01 +0100

    Add funkcje.h

---
+++ funkcje.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+void przyklady(){
+    string tab_przyklady[5];
+    for(int i==0; i<5; i++){
+        cin >> tab_przyklady[i];
+        cout << "\tTwój " << i << " przyklad to: " << tab_przyklady[i] << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <windows.h>
+#include <funkcje.h>
 using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "Przeżywamy trudny okres epidemii..\n W tych trudnych chwilach powinniśmy spędzaczas w domu. Z tego powodu proszę o podanie 5 przykładow co mozna robic zeby czas szybciej mijal...";
     cout << "\n Podaj 5 przykladow takich czynnosci a bede ci wdzieczny :)";
+    przyklady();
     return 0;
 }
